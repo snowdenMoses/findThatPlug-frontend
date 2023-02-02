82437b63e91ce6e60cecaf636731a552ec30a7df
snowdenmoses <111445710+AbubakarMoses@users.noreply.github.com>
2023-02-02 03:52:08 +0100

    publish branch

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -7,9 +7,10 @@ const AddUser = () => (
             <Form.Group widths='equal'>
                 <Form.Input fluid label='First name' placeholder='First name' />
                 <Form.Input fluid label='Last name' placeholder='Last name' />
+                <Form.Input fluid label='Email' placeholder='email address' />
+                <Form.Input fluid label='Password' placeholder='Password' />
             </Form.Group>
-            <Form.Checkbox label='I agree to the Terms and Conditions' />
-            <Button type='submit'>Submit</Button>
+            <Button type='submit'>Sign Up</Button>
         </Form>
     </Segment>
 )
